800288f1f713ba9924ffb3689b84b7984f1578a5
HeshamTB <hishaminv@gmail.com>
2021-03-02 23:01:30 +0300

    Added S, U and J. Now also writes out text/bin

--- clean.asm
+++ clean.asm
@@ -1,12 +1,15 @@
 main:
 	addi a1, zero, 0xff
+	sb t1, -100(t2)
 	addi t1, a1, 25
 	xor  t2, t1, a1
-	call print
+	lui t1, 500
+	jal ra, print
 print:
 	addi a0, zero, 1
 	add  a1, t1, zero
 	addi a2, zero, 32
+	j print
 	addi a7, zero, 64
 	beq a7, zero, print
 	ecall

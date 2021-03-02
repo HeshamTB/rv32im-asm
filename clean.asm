19fa2a99e0f450d3f0bf1a9c72dff317b5db6a11
HeshamTB <hishaminv@gmail.com>
2021-03-02 10:30:16 +0300

    A source file that is clean

---
+++ clean.asm
@@ -0,0 +1,13 @@
+main:
+	addi a1, zero, 25
+	addi t1, a1, 25
+	xor  t2, t1, a1
+	call print
+print:
+	addi a0, zero, 1
+	add  a1, t1, zero
+	addi a2, zero, 32
+	addi a7, zero, 64
+	beq a7, zero, print
+	ecall
+	

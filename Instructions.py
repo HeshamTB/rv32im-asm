d9e79154428c0b073f922cee2673fdfb07808754
AIB <ah444mad@hotmail.com>
2021-03-07 23:39:02 +0300

    Added Comments, cleaned main class and instructions class

--- Instructions.py
+++ Instructions.py
@@ -1,16 +1,9 @@
-# The class Instruction takes several inputs based on the format of the instruction. Then function to_binary returns
-# the binary representation.
-# However, only 'R' and 'I' types are working currently
 
-# TO DO:
-#   Very important: for imddeiate shift operations, we need to use only lower 5 bits (produce 32 shift)
-#   ... not like the tables that use lower 6 bits (they have 64 bit cpu, but we are designing 32-bit cpu)
-
-# for the immediate, i think we should sign extend it to 32 bits, then do whatever shifting we need.
-
-# -------------------------Class------------------------ #
 class Instruction:
-
+    """
+    Instruction class: takes several inputs based on the format of the instruction. Then function to_binary returns
+    the binary representation.
+    """
     # Init
     def __init__(self, instr, frmt='R', imm='0', func7='0', rs2='0', rs1='0', func3='0', rd='0'):
         self.instr = instr
@@ -22,13 +15,22 @@ class Instruction:
         self.rd = rd
         self.frmt = frmt.capitalize()
 
-    # return binary
+    # return binary representation
     def to_binary(self):
+        """
+        A function that organize converts the passed arguments into a binary representation and returns it to caller.
+        :return: binary representation of a line of instructions
+        """
+
+        # Note: The notation    SomeList[::-1]      just inverts the list
+        # we use this feature to be able to deal with lists like a hardware description language
+        # without using myHDL library
+
         if self.frmt == 'R':
             opcode = '0110011'
             return self.func7 + self.rs2 + self.rs1 + self.func3 + self.rd + opcode
 
-        elif self.frmt == 'I':  # Fixed, but slli-srli=srai might need better solution.
+        elif self.frmt == 'I':
             if self.instr[0] == 'l':  # A load instruction
                 opcode = '0000011'
             elif self.instr == 'jalr':
@@ -55,12 +57,10 @@ class Instruction:
             upper_imm = temp[11:4:-1]  # from bit 11 to bit 5 (4 is not included)
             return upper_imm + self.rs2 + self.rs1 + self.func3 + lower_imm + opcode
 
-        elif self.frmt == 'B':  # Fixed
+        elif self.frmt == 'B':
             opcode = '1100011'
-            # Note: The immediate will be 12 bit signed binary. we will sign-extend it one bit left, then choose ...
             # only bits 12:1 excluding bit 0, which is always equal to 0
             temp = self.imm[::-1]
-            #temp = temp[11] + temp  # set bit 12 to be the same as bit 11 (sign extension by one bit)
             imm_11 = temp[11]
             imm_4 = temp[4:0:-1]  # from bit 4 to bit 1 (0 is not included)
             imm_10 = temp[10:4:-1]  # from bit 10 to bit 5 (4 is not included)
@@ -86,7 +86,7 @@ class Instruction:
 
 
 # ----------------------------- Main Dictionary ----------------------- #
-# This nested dictionary contains all the instructions and their types
+# This nested dictionary contains all the instructions and their types, and other fixed information
 all_instructions = {'R': [{'inst': 'add', 'func3': '000',  'func7': '0000000'},
                           {'inst': 'sub', 'func3': '000',  'func7': '0100000'},
                           {'inst': 'xor', 'func3': '100',  'func7': '0000000'},
@@ -132,29 +132,29 @@ all_instructions = {'R': [{'inst': 'add', 'func3': '000',  'func7': '0000000'},
 
                     'J': [{'inst': 'jal'}]}
 
-if __name__ == '__main__':
+# ----------------------- Testing Area ------------------------ #
 
-    # ----------------------- Testing Area ------------------------ #
-    test = 'add'
+if __name__ == '__main__':
+    test = ''
     for key, value in all_instructions.items():  # loop over all types
         for v in value:  # loop over all instructions inside a certain type
             if v['inst'] == test:  # if the value corresponding to the key 'inst' is identical to the input:
-                # Pass the data to class Instruction based on format (for example frmt J doesn't need func3 but R needs it)
-                # Then print the binary
                 if key == 'R':
-                    print(Instruction(instr=v['inst'], frmt=key, func7=v['func7'], rs2='00010', rs1='00001', func3=v['func3'],
-                                      rd='00000').to_binary())  # rd, rs1, rs2 are user inputs
+                    print(Instruction(instr=v['inst'], frmt=key, func7=v['func7'], rs2='00010',
+                                      rs1='00001', func3=v['func3'],
+                                      rd='00000').to_binary())
                 elif key == 'I':
-                    print(Instruction(instr=v['inst'], frmt=key, imm='00000000001', rs1='00001', func3=v['func3'], rd='00000').to_binary())  # rd,rs1,imm are user inputs
+                    print(Instruction(instr=v['inst'], frmt=key, imm='00000000001', rs1='00001',
+                                      func3=v['func3'], rd='00000').to_binary())
                 elif key == 'S':
-                    # rd, rs1, rs2, imm are user inputs
-                    print(Instruction(instr=v['inst'], frmt=key, imm='00000000001', rs2='10000', rs1='00001', func3=v['func3'],
+                    print(Instruction(instr=v['inst'], frmt=key, imm='00000000001', rs2='10000',
+                                      rs1='00001', func3=v['func3'],
                                       rd='00000').to_binary())
                 elif key == 'B':
-                    # rd, rs1, rs2, imm are user inputs
-                    print(Instruction(instr=v['inst'], frmt=key, imm='00000000001', rs2='10000', rs1='00001', func3=v['func3'],
+                    print(Instruction(instr=v['inst'], frmt=key, imm='00000000001', rs2='10000',
+                                      rs1='00001', func3=v['func3'],
                                       rd='00000').to_binary())
                 elif key == 'U':
-                    print(Instruction(instr=v['inst'], frmt=key, imm='00000000001', rd='00000').to_binary())  # rd, imm are user inputs
+                    print(Instruction(instr=v['inst'], frmt=key, imm='00000000001', rd='00000').to_binary())
                 elif key == 'J':
-                    print(Instruction(instr=v['inst'], frmt=key, imm='00000000001', rd='00000').to_binary())  # rd, imm are user inputs
+                    print(Instruction(instr=v['inst'], frmt=key, imm='00000000001', rd='00000').to_binary())
